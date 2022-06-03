e0dca9171e372f14307c6589c191ddcbef2e437e
Lauri Havi <72817588+L-Havi@users.noreply.github.com>
2022-06-04 00:20:52 +0300

    Create subdomain_mapper.py

---
+++ Subdomains/subdomain_mapper.py
@@ -0,0 +1,21 @@
+ /usr/bin/env python
+
+import requests
+
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+target_url = "insert a domain you are mapping here"
+
+with open("subdomains-wordlist.txt", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = target_url + "/" + word
+        response = request(test_url)
+        if response:
+            print("[+] Discovered subdomain --> " + test_url)
